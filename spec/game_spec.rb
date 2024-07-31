fdeb2a7fba63df22bfbad012b0ef470dcc2308d0
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-07-31 16:37:42 +0100

    Create rspec test for message conditional (currently fails)

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,20 +7,32 @@ describe Game do
     before do
       allow(game).to receive(:puts)
       allow(game).to receive(:gets).and_return("Pete")
-      game.request_name
     end
 
     context "when no player name has been assigned yet" do
       it "assigns name to player 1" do
+        game.request_name
         expect(game.player1[:name]).to eq("Pete")
+      end
+
+      it "asks for name of player 1" do
+        expect(game).to receive(:puts).with("Please enter name of player 1:")
+        game.request_name
       end
     end
 
     context "when player1 has name assigned" do
       it "assigns name to player 2" do
+        game.request_name
         allow(game).to receive(:gets).and_return("Carl")
         game.request_name
         expect(game.player2[:name]).to eq("Carl")
+      end
+
+      it "asks for name of player 2" do
+        game.request_name
+        expect(game).to receive(:puts).with("Please enter name of player 2:")
+        game.request_name
       end
     end
   end
