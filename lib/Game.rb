15a3326e85a9df8f755423ccf4e41ab3682d00fe
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-01 11:44:16 +0100

    Creating @board variable and #visualize_board method

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,15 +1,17 @@
 class Game
-  attr_accessor :player1, :player2
+  attr_accessor :player1, :player2, :board
   attr_reader :available_tokens
 
   def initialize
     @player1 = { name: nil, token: nil }
     @player2 = { name: nil, token: nil }
     @available_tokens = ["\u232C", "\u25A9"]
+    @board = Array.new(6) { Array.new(7, " ") }
   end
 
   def play_game
     create_players
+    visualizing_board
   end
 
   def create_players
@@ -51,6 +53,17 @@ class Game
       player_name = gets.chomp
       player2[:name] = player_name
     end
+  end
+
+  def visualizing_board
+    puts "| #{board[5][0]} | #{board[5][1]} | #{board[5][2]} | #{board[5][3]} | #{board[5][4]} | #{board[5][5]} | #{board[0][6]} |"
+    puts "| #{board[4][0]} | #{board[4][1]} | #{board[4][2]} | #{board[4][3]} | #{board[4][4]} | #{board[4][5]} | #{board[4][6]} |"
+    puts "| #{board[3][0]} | #{board[3][1]} | #{board[3][2]} | #{board[3][3]} | #{board[3][4]} | #{board[3][5]} | #{board[3][6]} |"
+    puts "| #{board[2][0]} | #{board[2][1]} | #{board[2][2]} | #{board[2][3]} | #{board[2][4]} | #{board[2][5]} | #{board[2][6]} |"
+    puts "| #{board[1][0]} | #{board[1][1]} | #{board[1][2]} | #{board[1][3]} | #{board[1][4]} | #{board[1][5]} | #{board[1][6]} |"
+    puts "| #{board[0][0]} | #{board[0][1]} | #{board[0][2]} | #{board[0][3]} | #{board[0][4]} | #{board[0][5]} | #{board[0][6]} |"
+    puts " \u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E\u203E"
+    puts "  1   2   3   4   5   6   7  "
   end
 
   private
