1169fede437431eb3e76938e86476e3344d57a36
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-01 09:55:06 +0100

    Expand #assign_token test to check for the case player 1 choses the second token

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -38,31 +38,63 @@ describe Game do
   end
 
   describe "assign_token" do
-    before do
-      allow(game).to receive(:puts)
-      allow(game).to receive(:player_input_token).and_return(0)
-      game.assign_token
+    context "when player1 chooses first token (\u232C)" do
+      before do
+        allow(game).to receive(:puts)
+        allow(game).to receive(:player_input_token).and_return(0)
+        game.assign_token
+      end
+
+      context "when no token has been assigned" do
+        it "assigns chosen token to player 1" do
+          expect(game.player1[:token]).to eq("\u232C")
+        end
+
+        it "removes chosen token from @available_tokens array" do
+          expect(game.available_tokens).to eq(["\u25A9"])
+        end
+      end
+
+      context "when a token was already chosen by player 1" do
+        it "assigns remaining token to player 2" do
+          game.assign_token
+          expect(game.player2[:token]).to eq("\u25A9")
+        end
+
+        it "removes last remaining token from @available_tokens array" do
+          game.assign_token
+          expect(game.available_tokens).to eq([])
+        end
+      end
     end
 
-    context "when no token has been assigned" do
-      it "assigns chosen token to player 1" do
-        expect(game.player1[:token]).to eq("\u232C")
-      end
-
-      it "removes chosen token from @available_tokens array" do
-        expect(game.available_tokens).to eq(["\u25A9"])
-      end
-    end
-
-    context "when a token was already chosen by player 1" do
-      it "assigns remaining token to player 2" do
+    context "when player1 chooses second token (\u25A9)" do
+      before do
+        allow(game).to receive(:puts)
+        allow(game).to receive(:player_input_token).and_return(1)
         game.assign_token
-        expect(game.player2[:token]).to eq("\u25A9")
       end
 
-      it "removes last remaining token from @available_tokens array" do
-        game.assign_token
-        expect(game.available_tokens).to eq([])
+      context "when no token has been assigned" do
+        it "assigns chosen token to player 1" do
+          expect(game.player1[:token]).to eq("\u25A9")
+        end
+
+        it "removes chosen token from @available_tokens array" do
+          expect(game.available_tokens).to eq(["\u232C"])
+        end
+      end
+
+      context "when a token was already chosen by player 1" do
+        it "assigns remaining token to player 2" do
+          game.assign_token
+          expect(game.player2[:token]).to eq("\u232C")
+        end
+
+        it "removes last remaining token from @available_tokens array" do
+          game.assign_token
+          expect(game.available_tokens).to eq([])
+        end
       end
     end
   end
