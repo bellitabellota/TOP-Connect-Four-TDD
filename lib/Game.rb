ef1cd59404d59ed3429dfa4a7fe99d2593885da3
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-02 17:29:29 +0100

    Fix bug in #visualizing_board to correct value displayed in last slot of column 7

--- lib/Game.rb
+++ lib/Game.rb
@@ -131,7 +131,7 @@ class Game
 
   def visualizing_board
     puts
-    puts "| #{board[5][0]} | #{board[5][1]} | #{board[5][2]} | #{board[5][3]} | #{board[5][4]} | #{board[5][5]} | #{board[0][6]} |"
+    puts "| #{board[5][0]} | #{board[5][1]} | #{board[5][2]} | #{board[5][3]} | #{board[5][4]} | #{board[5][5]} | #{board[5][6]} |"
     puts "| #{board[4][0]} | #{board[4][1]} | #{board[4][2]} | #{board[4][3]} | #{board[4][4]} | #{board[4][5]} | #{board[4][6]} |"
     puts "| #{board[3][0]} | #{board[3][1]} | #{board[3][2]} | #{board[3][3]} | #{board[3][4]} | #{board[3][5]} | #{board[3][6]} |"
     puts "| #{board[2][0]} | #{board[2][1]} | #{board[2][2]} | #{board[2][3]} | #{board[2][4]} | #{board[2][5]} | #{board[2][6]} |"
