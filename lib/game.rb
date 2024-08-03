59a288797cc6763aed31b3272ad1cf7f6c053155
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-03 16:09:40 +0100

    Separate methods related to player move into a module

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require_relative "winning_patterns"
+require_relative "player_move"
 
 class Game
   attr_accessor :player1, :player2, :board, :current_player, :last_token_position
@@ -14,6 +15,7 @@ class Game
   end
 
   include WinningPattern
+  include PlayerMove
 
   def play_game
     create_players
@@ -42,54 +44,6 @@ class Game
       self.current_player = player2
     else
       self.current_player = player1
-    end
-  end
-
-  def make_player_move(player)
-    puts
-    puts "#{player[:name]}, please make your move by entering the number of the column in which you want to place your token:"
-    next_move = verified_move
-    place_token(next_move)
-  end
-
-  def place_token(next_move)
-    index_row = find_free_slot(next_move)
-    update_free_slot_with_player_token(index_row, next_move)
-  end
-
-  def update_free_slot_with_player_token(index_row, next_move)
-    board[index_row][next_move - 1] = current_player[:token]
-    last_token_position[0] = index_row
-    last_token_position[1] = next_move - 1
-  end
-
-  def find_free_slot(next_move)
-    board.each_with_index { |array, index| break index if array[next_move - 1] == " " }
-  end
-
-  def verified_move
-    loop do
-      next_move = request_next_move
-
-      return next_move if column_has_empty_slot?(next_move)
-
-      puts "Column already complete. Please choose another column:"
-    end
-  end
-
-  def column_has_empty_slot?(next_move)
-    index_column = next_move - 1
-    board.each { |array| return true if array[index_column] == " " }
-    false
-  end
-
-  def request_next_move
-    loop do
-      input = gets.chomp
-
-      return input.to_i if input.match?(/^\d+$/) && (1..7).include?(input.to_i)
-
-      puts "Invalid input. Please enter a number between 1 and 7:"
     end
   end
 
