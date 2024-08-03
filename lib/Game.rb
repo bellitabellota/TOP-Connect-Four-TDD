1059500cd6cbe06d76727348858ed58725c09208
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-03 10:59:13 +0100

    Complete #diagonal_pattern? and create #horizonntal_pattern?

--- lib/Game.rb
+++ lib/Game.rb
@@ -28,12 +28,23 @@ class Game
     
   end
 
-  def diagonal_pattern?
-    if from_lower_left_to_upper_right? || from_lower_right_to_upper_left?
-      true
-    else
-      false
+  def horizontal_pattern?
+    index1 = last_token_position[0]
+    token = current_player[:token]
+
+    row = board[index1]
+
+    case row
+    in ["#{token}", "#{token}", "#{token}", "#{token}", _, _, _] then true
+    in [_, "#{token}", "#{token}", "#{token}", "#{token}", _, _] then true
+    in [_, _, "#{token}", "#{token}", "#{token}", "#{token}", _] then true
+    in [_, _, _, "#{token}", "#{token}", "#{token}", "#{token}"] then true
+    else false
     end
+  end
+
+  def diagonal_pattern?
+    from_lower_left_to_upper_right? || from_lower_right_to_upper_left?
   end
 
   def from_lower_right_to_upper_left?
